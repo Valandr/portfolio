5bf26c262e6c1f29ae77e201dbcbaead96bd70fb
Andriani <valerie.andriani@gmail.com>
2020-02-24 16:40:10 +0100

    Adding views and routes for actions controller portfolio-blog-about-contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#welcome'
+  get '/portfolio' => 'pages#portfolio'
+  get '/blog' => 'pages#blog'
+  get '/about' => 'pages#about'
+  get '/contact' => 'pages#contact'
 end
